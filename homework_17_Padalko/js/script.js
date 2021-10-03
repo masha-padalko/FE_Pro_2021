0c286f01668ab2d010b021738c5c893253c97a10
masha-padalko <masha-padalko@users.noreply.github.com>
2021-10-03 16:03:15 +0300

    homework 17
    marvel dc heroes

---
+++ homework_17_Padalko/js/script.js
@@ -0,0 +1,51 @@
+let marvelHeroes = [
+	{
+		name: "Thor"
+	},
+	{
+		name: "Spider Man"
+	}
+];
+
+let dcHeroes = [
+	{
+		name: "Superman"
+	},
+	{
+		name: "Batman"
+	},
+	{
+		name: "Deadpool"
+	}
+];
+
+Array.prototype.heroesRender = function(folder) {
+
+	let trs = this
+		.map(function(hero){
+			return `<tr>
+				<td align="center">${hero.name}</td>
+				<td align="center">
+					<img src="images/${folder}/${(hero.name).replaceAll(' ', '')}.svg" width="30" height="30">
+				</td>
+			</tr>`;
+		})
+		.join(``);
+
+		let table = `<h2>${folder} heroes </h2><table border="1" width="400px">
+			<thead>
+				<tr>
+					<th align="center">Name</th>
+					<th align="center">Icon</th>
+				</tr>
+			</thead>
+			<tbody>
+				${trs}
+			</tbody>
+		</table>`
+
+	return table;
+}
+
+document.write(dcHeroes.heroesRender('dc'));
+document.write(marvelHeroes.heroesRender('marvel'));
